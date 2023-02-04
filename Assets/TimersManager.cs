541756805d939a4af11a401f5fcb676b9948b7b3
Aliaksei Mukharski <alex.mukharski@vizor-games.com>
2023-02-04 20:19:45 +0100

    Add GameMode and Tree objects to scene, add timers manager and tree controller

---
+++ Assets/TimersManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimersManager : MonoBehaviour
+{
+    [SerializeField] public float[] TreeStagesDurations;
+    private IEnumerator TreeStageCoroutine = null;
+
+    private IEnumerator StartGameAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        print("Started");
+    }
+
+    public void RequestStartTreeStage(int stage)
+    {
+        if (TreeStageCoroutine != null)
+        {
+            print("TreeStage coroutine already active");
+            return;
+        }
+
+        TreeStageCoroutine = StartTreeStage(stage);
+    }
+
+    private IEnumerator StartTreeStage(int stage)
+    {
+        print("Tree stage " + stage + " started");
+        yield return new WaitForSeconds(TreeStagesDurations[stage]);
+        OnTreeStageEnded(stage);
+    }
+
+    public void OnTreeStageEnded(int stage)
+    {
+        TreeStageCoroutine = null;
+        print("Tree stage " + stage + " ended");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IEnumerator coroutine = StartGameAfterTime(10);
+        StartCoroutine(coroutine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/TreeController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeController : MonoBehaviour
+{
+    [SerializeField] private Sprite[] sprites = null;
+
+    void startGame()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
