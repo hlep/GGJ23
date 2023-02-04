2b423951436816ac59f1df0f6b77869dda882237
Aliaksei Mukharski <alex.mukharski@vizor-games.com>
2023-02-04 21:40:49 +0100

    Move tree timers to tree controlled, move island to better fit tree

--- Assets/TreeController.cs
+++ Assets/TreeController.cs
@@ -1,10 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D.Animation;
 
 public class TreeController : MonoBehaviour
 {
-    [SerializeField] private Sprite[] sprites = null;
+    [SerializeField] public float[] TreeStagesDurations;
+    [SerializeField] private SpriteResolver spriteResolver;
+    [SerializeField] private SpriteLibrary spriteLibrary;
+    [SerializeField] private string spriteLibraryCategoryName;
+
+    private IEnumerator TreeStageCoroutine = null;
+
+    public void RequestStartTreeStage(int stage)
+    {
+        if (TreeStageCoroutine != null)
+        {
+            print("TreeStage coroutine already active");
+            return;
+        }
+
+        TreeStageCoroutine = StartTreeStage(stage);
+    }
+
+    private IEnumerator StartTreeStage(int stage)
+    {
+        print("Tree stage " + stage + " started");
+        yield return new WaitForSeconds(TreeStagesDurations[stage]);
+        OnTreeStageEnded(stage);
+    }
+
+    public void OnTreeStageEnded(int stage)
+    {
+        TreeStageCoroutine = null;
+        print("Tree stage " + stage + " ended");
+    }
 
     void startGame()
     {
