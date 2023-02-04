16823d824bc25d27114b191e31200d7e60333aeb
nikita.gursky <nikita.gursky@vizor-games.com>
2023-02-05 01:00:58 +0300

    Change mouse overlap method

--- Assets/ClickCatcher.cs
+++ Assets/ClickCatcher.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.U2D;
+using UnityEngine.UIElements;
 
 public class ClickCatcher : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class ClickCatcher : MonoBehaviour
     Vector3 m_SavedClick;
     bool m_ClickActive = false;
     public GameObject rootPrefab;
-    int m_LatestLayer = 0;
+    //int m_LatestLayer = 0;
 
     [SerializeField]
     LineTracker m_LineTracker;
@@ -30,21 +31,29 @@ public class ClickCatcher : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 mousePosition = Input.mousePosition;
-            Ray ray = m_Camera.ScreenPointToRay(mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
-            if (hit)
+            mousePosition.z = 0.0f;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            var collider = Physics2D.OverlapPoint(mousePosition);
+            //if (collider)
+          
+
+            //Ray ray = m_Camera.ScreenPointToRay(mousePosition);
+            //RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+            if (collider)
             {
-                if (m_ClickActive && hit.collider.gameObject.tag == "Earth")
+                if (m_ClickActive && collider.gameObject.tag == "Earth")
                 {
                     if (!m_ActionsTracker.HasFreeActions())
                     {
                         return;
                     }
 
-                    if (m_LineTracker.CheckLineIntersect(m_SavedClick, ray.origin))
+                    if (m_LineTracker.CheckLineIntersect(m_SavedClick, mousePosition))
                     {
                         //do some warning?
                         StopClick();
+                        print("Line intersect fail...");
                         return;
                     }
 
@@ -52,33 +61,33 @@ public class ClickCatcher : MonoBehaviour
 
                     GameObject SpawnedRoot = Instantiate(rootPrefab, Vector3.zero, Quaternion.identity);
 
-                    Vector3 pos = SpawnedRoot.transform.position;
-                    SpawnedRoot.transform.position.Set(pos.x, pos.y, m_LatestLayer++);
+                    //Vector3 pos = SpawnedRoot.transform.position;
+                    //SpawnedRoot.transform.position.Set(pos.x, pos.y, m_LatestLayer++);
 
                     SpriteShapeController shapeController = SpawnedRoot.GetComponent<SpriteShapeController>();
                     shapeController.spline.SetPosition(0, m_SavedClick);
 
                     //we can't spawn two points at the same place, so here's that
-                    shapeController.spline.SetPosition(1, Vector3.Lerp(m_SavedClick, ray.origin, 0.2f));
+                    shapeController.spline.SetPosition(1, Vector3.Lerp(m_SavedClick, mousePosition, 0.2f));
 
                     RootGrower grower = SpawnedRoot.GetComponent<RootGrower>();
-                    grower.SetRootEndpoint(ray.origin);
+                    grower.SetRootEndpoint(mousePosition);
                     grower.growEndDelegate = EndGrow;
 
                     grower.m_EnergyManager = m_EnergyManager;
 
-                    m_LineTracker.AddNewLine(m_SavedClick, ray.origin);
+                    m_LineTracker.AddNewLine(m_SavedClick, mousePosition);
 
                     m_ActionsTracker.UpdateActions(-1);
 
                     StopClick();
                 }
 
-                if (hit.collider.gameObject.tag == "Root")
+                if (collider.gameObject.tag == "Root")
                 {
 
                     m_ClickActive = true;
-                    m_SavedClick = ray.origin;
+                    m_SavedClick = mousePosition;
                     //start showing how much energy would we lose?
                 }
             }
