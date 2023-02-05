3b13199cca8e8cf35de8b8df5fb47b20587d33e0
Aliaksei Mukharski <alex.mukharski@vizor-games.com>
2023-02-05 14:18:11 +0100

    Add tree fall off logic and sprites

--- Assets/TreeController.cs
+++ Assets/TreeController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.U2D.Animation;
 
@@ -18,10 +19,21 @@ public class TreeController : MonoBehaviour
     [SerializeField] private SpriteResolver spriteResolver;
     [SerializeField] private SpriteLibrary spriteLibrary;
     [SerializeField] private TreeStageSetup[] stagesSetup;
+    public float[] weightDifferenceForBreakStage;
+    public float weightDifferenceForLose;
 
-    private bool bTreeStarted = false;
-    private int currentTreeStage;
-    private float currentWeight;
+    private Vector3 StartTreePosition;
+    [SerializeField] private Vector3 LoseTreePosition;
+    [SerializeField] private float LoseTime;
+
+    private bool bTreeWorking = false;
+    private bool bIsLosingInProgress = false;
+    private bool bLost = false;
+
+    private int currentTreeStage = -1;
+    private int currentBreakStage = -1;
+    private float currentWeight = 0;
+    private float compensatedWeight = 0;
 
     private Coroutine TreeStageCoroutine = null;
 
@@ -69,15 +81,83 @@ public class TreeController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Tree not started!!!");
-        // startTree();
+        // Debug.Log("Tree not started!!!");
+        StartTreePosition = transform.position;
+        bTreeWorking = true;
+        startTree();
     }
 
-    private float lastWeightUpdateTime;
+    void LoseGame()
+    {
+        StopCoroutine(TreeStageCoroutine);
+        loseStartTime = Time.time;
+        bIsLosingInProgress = true;
+        bTreeWorking = false;
+    }
+
+    private float loseStartTime;
+    void UpdateLosePosition()
+    {
+        float timeSinceStart = Time.time - loseStartTime;
+        float finishPercent = timeSinceStart / LoseTime;
+        if (finishPercent > 1)
+        {
+            transform.position = LoseTreePosition;
+            bIsLosingInProgress = false;
+            bLost = true;
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(StartTreePosition, LoseTreePosition, finishPercent);
+        }
+    }
+
+    public void UpdateBreakStage()
+    {
+        spriteResolver.SetCategoryAndLabel(spriteResolver.GetCategory(), stagesSetup[currentTreeStage].brokenSpriteLabels[currentBreakStage]);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (bIsLosingInProgress)
+        {
+            UpdateLosePosition();
+        }
+
+        if (!bTreeWorking)
+        {
+            return;
+        }
+
+        if (currentTreeStage < 0 || currentTreeStage >= stagesSetup.Length)
+        {
+            return;
+        }
+
+        TreeStageSetup currentStageSetup = stagesSetup[currentTreeStage];
+        if (currentStageSetup.brokenSpriteLabels.Length > 0)
+        {
+            if (currentWeight - compensatedWeight > weightDifferenceForLose)
+            {
+                LoseGame();
+                return;
+            }
+
+            int nextBreakStage = currentBreakStage + 1;
+            if (nextBreakStage < currentStageSetup.brokenSpriteLabels.Length)
+            {
+                if (currentWeight - compensatedWeight > weightDifferenceForBreakStage[nextBreakStage])
+                {
+                    currentBreakStage++;
+                }
+            }
+
+            UpdateBreakStage();
+        }
+        else if (currentBreakStage != -1)
+        {
+            currentBreakStage = -1;
+        }
     }
 }
